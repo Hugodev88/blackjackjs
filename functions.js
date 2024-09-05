cc4eb7287561b244320119206b6e3fd747cb9a0b
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2024-09-05 18:01:05 -0300

    novas mudancas

--- functions.js
+++ functions.js
@@ -14,7 +14,7 @@ function embaralharBaralho(cartas) {
 }
 
 let baralho = embaralharBaralho(cartas);
-let parou = 0
+let parou = 0;
 let cartasJogador = [];
 let cartasMesa = [];
 
@@ -24,35 +24,38 @@ const visorCartasMesa = document.querySelector(".visorCartasMesa");
 const visorCartasMesaTotal = document.querySelector(".visorCartasMesaTotal");
 const visorCartasJogador = document.querySelector(".visorCartasJogador");
 const visorCartasJogadorTotal = document.querySelector(".visorCartasJogadorTotal");
-const statusDoJogo = document.querySelector(".status")
+const statusDoJogo = document.querySelector(".status");
+statusDoJogo.style.color = "white"
 
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
 
 button1.onclick = comecar;
 button2.onclick = darCartasJogador;
 button3.onclick = parar;
 button4.onclick = resetar;
 
+visorBaralho.innerHTML = baralho.length
+
 function comecar(){
-    statusDoJogo.innerHTML = "Novo jogo" 
+    statusDoJogo.innerHTML = "Novo jogo";
     cartasJogador.push(baralho.pop());
-    darCartasMesa()
+    darCartasMesa();
     cartasJogador.push(baralho.pop());
-    parou = 1
-    atualizarVisor()
-    parou = 0
-    darCartasMesa()
+    parou = 1;
+    atualizarVisor();
+    parou = 0;
+    darCartasMesa();
 }
 
 function darCartasJogador(){
     cartasJogador.push(baralho.pop());
-    console.log(cartasJogador+"\n");
-    atualizarVisor()
+    atualizarVisor();
     if (contarCartas(cartasJogador) > 21) {
-        parou = 1
-        atualizarVisor()
+        parou = 1;
+        atualizarVisor();
         perdeu();
     }
 }
@@ -62,70 +65,74 @@ function darCartasMesa(){
 }
 
 function parar(){
-    parou = 1
+    parou = 1;
     while(contarCartas(cartasMesa) < 17){
         cartasMesa.push(baralho.pop());
-        atualizarVisor()
+        atualizarVisor();
     } 
     verificar(contarCartas(cartasJogador), contarCartas(cartasMesa));
 }
 
 function verificar(cartasJogador, cartasMesa){
     if(cartasJogador > 21){
-        parou = 1
-        atualizarVisor()
-        perdeu()
+        parou = 1;
+        atualizarVisor();
+        perdeu();
     } else if (cartasMesa > 21){
-        atualizarVisor()
-        ganhou()
+        atualizarVisor();
+        ganhou();
     } else if (cartasJogador <= 21 && cartasJogador > cartasMesa){
-        atualizarVisor()
-        ganhou()
+        atualizarVisor();
+        ganhou();
     } else if (cartasMesa <= 21 && cartasMesa > cartasJogador){
-        atualizarVisor()
-        perdeu()
+        atualizarVisor();
+        perdeu();
     } else {
-        atualizarVisor()
-        empate()
+        atualizarVisor();
+        empate();
     }
 }
 
 function contarCartas(cartas){
     let contagem = 0;
-    for(let i = 0; i < cartas.length ;i++){
+    for(let i = 0; i < cartas.length; i++){
         contagem += cartas[i];
     }
     return contagem;
 }
 
 function ganhou() {
-    statusDoJogo.innerHTML = "Voce ganhou." 
+    statusDoJogo.innerHTML = "Você ganhou."; 
+    statusDoJogo.style.color = "green"
 }
 
 function empate() {
-    statusDoJogo.innerHTML = "Voce empatou." 
+    statusDoJogo.innerHTML = "Você empatou."; 
+    statusDoJogo.style.color = "yellow"
 }
 
 function perdeu() {
-    statusDoJogo.innerHTML = "Voce perdeu." 
+    statusDoJogo.innerHTML = "Você perdeu."; 
+    statusDoJogo.style.color = "red"
 }
 
 function atualizarVisor(){
-
     if (parou == 1){
-        visorCartasMesa.innerHTML = cartasMesa
-        visorCartasMesaTotal.innerHTML = contarCartas(cartasMesa)
+        visorCartasMesa.innerHTML = cartasMesa.join(' | ');  // Exibe as cartas da mesa separadas por espaço
+        visorCartasMesaTotal.innerHTML = contarCartas(cartasMesa);
     }
-   
-    visorCartasJogador.innerHTML = cartasJogador
-    visorCartasJogadorTotal.innerHTML = contarCartas(cartasJogador)
+    visorCartasJogador.innerHTML = cartasJogador.join(' | ');  // Exibe as cartas do jogador separadas por espaço
+    visorCartasJogadorTotal.innerHTML = contarCartas(cartasJogador);
+
+    visorBaralho.innerHTML = baralho.length
 }
 
 function resetar(){
-    baralho = embaralharBaralho(cartas)
+    baralho = embaralharBaralho(cartas);
     cartasJogador = [];
     cartasMesa = [];
-    statusDoJogo.innerHTML = "Novo jogo" 
-    atualizarVisor()
-    parou = 0
+    statusDoJogo.innerHTML = "Novo jogo";
+    atualizarVisor();
+    parou = 0;
+    statusDoJogo.style.color = "white"
 }
